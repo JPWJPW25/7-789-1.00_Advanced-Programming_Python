f31740d51c87e46218d6d296fc5e4b94ad3aebeb
Jan-Philipp Wittmann <52930365+JPWJPW25@users.noreply.github.com>
2021-12-26 19:41:31 +0100

    Delete Word2txt.py

--- Word2txt.py
+++
@@ -1,8 +0,0 @@
-import docx2txt
-
-# Passing docx file to process function
-text = docx2txt.process("test.docx")
-
-# Saving content inside docx file into output.txt file
-with open("output.txt", "w") as text_file:
-	print(text, file=text_file)
